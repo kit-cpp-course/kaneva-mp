753e0dcb864c5d3f50ac72e5535c2c6ebd69c338
kaneva-mp <46100740+kaneva-mp@users.noreply.github.com>
2018-12-27 23:16:11 +0300

    Update triangle.hpp

--- triangle.hpp
+++ triangle.hpp
@@ -29,13 +29,7 @@ public:
      *
      * @returns результат векторного произведения
      */
-    static double cross_prod (point * first, point * second, point * third) {
-        double  x1 = second->getX() - first->getX(),
-                x2 = third->getX() - first->getX(),
-                y1 = second->getY() - first->getY(),
-                y2 = third->getY() - first->getY();
-        return (x1 * y2 - x2 * y1);
-    }
+    static double cross_prod (point * first, point * second, point * third);
     /*
      * Проверка на принадлежность каждой точки треугольнику
      *
@@ -44,28 +38,10 @@ public:
      *
      * @returns результат проверки
      */
-    static bool is_in_triangle (point * first, point * second, point * third, point * cur) {
-        double a, b, c;
-        point *tmp = cur;
-        do {
-            if ( tmp != first && tmp != second && tmp != third ) {
-                a = cross_prod(tmp, first, second);
-                b = cross_prod(tmp, second, third);
-                c = cross_prod(tmp, third, first);
-                if ( ( a > 0 && b > 0 && c > 0 ) || ( a < 0 && b < 0 && c < 0 ) )
-                    return true;
-            }
-            tmp = tmp->next;
-        } while ( tmp != cur );
-        return false;
-    }
+    static bool is_in_triangle (point * first, point * second, point * third, point * cur);
     /*
-     * Вывод в консоль координат вершин треугольника
+     * Вывод координат вершин треугольника
      */
-    std::string printPoints() {
-        std::string dots = "";
-        for (int i = 0; i < points.size(); i++)
-            dots += ("(" + std::to_string(points[i].getX()) + ";" + std::to_string(points[i].getY()) + ")" + " ");
-        return dots;
-    }
+    std::string printPoints();
 };
+
